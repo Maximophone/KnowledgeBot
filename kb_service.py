8ba0bf7b002370c485e48f232610b298da505623
Maxime Fournes <fournes.maxime@gmail.com>
2025-01-06 09:33:13 +0100

    removed commented out code

--- kb_service.py
+++ kb_service.py
@@ -139,27 +139,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# import asyncio
-# from obsidian_ai import process_file, needs_answer, VAULT_PATH
-# from file_watcher import start_file_watcher
-# from transcribe import (
-#     create_required_directories,
-#     start_repeaters
-# )
-
-# async def run_obsidian_ai():
-#     await start_file_watcher(VAULT_PATH, process_file, needs_answer, use_polling=True)
-
-# async def run_transcribe_services():
-#     create_required_directories()
-#     await start_repeaters()
-
-# async def main():
-#     obsidian_ai_task = asyncio.create_task(run_obsidian_ai())
-#     transcribe_task = asyncio.create_task(run_transcribe_services())
-
-#     await asyncio.gather(obsidian_ai_task, transcribe_task)
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
