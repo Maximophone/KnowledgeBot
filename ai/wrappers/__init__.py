c5587740dcfe14c9cc481907712ca67c59c2b49b
Maxime Fournes <fournes.maxime@gmail.com>
2025-01-24 18:12:58 +0100

    Added perplexity wrapper, and flashthinking gemini model

--- ai/wrappers/__init__.py
+++ ai/wrappers/__init__.py
@@ -4,13 +4,15 @@ from .google import GeminiWrapper
 from .openai import GPTWrapper
 from .mock import MockWrapper
 from .deepseek import DeepSeekWrapper
+from .perplexity import PerplexityWrapper
 
 __all__ = [
     'AIWrapper',
     'AIResponse',
-    'ClaudeWrapper',
-    'GeminiWrapper',
-    'GPTWrapper',
     'MockWrapper',
-    'DeepSeekWrapper'
+    'ClaudeWrapper',
+    'GPTWrapper',
+    'GeminiWrapper',
+    'DeepSeekWrapper',
+    'PerplexityWrapper',
 ]
